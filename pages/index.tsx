6d85eb9b8a9556276a99eb9fd5169bdbdd3b4acd
Frank Hartman <frank.hartman@lightyear.one>
2022-06-20 15:02:28 +0200

    Add NextJS and TailwindCSS

---
+++ pages/index.tsx
@@ -0,0 +1,11 @@
+import type { NextPage } from "next";
+
+const Home: NextPage = () => {
+  return (
+    <div className="w-full min-h-screen bg-blue-50 flex items-center justify-center">
+      <h1 className="font-bold text-4xl">Hello world!</h1>
+    </div>
+  );
+};
+
+export default Home;
