1408eb08954635d99be3bdff12a681e608241b36
Frank Hartman <frank.hartman@lightyear.one>
2022-06-20 16:14:17 +0200

    Add react three fiber

--- README.md
+++ README.md
@@ -3,9 +3,12 @@
 First, run the development server:
 
 ```bash
+npm install
 npm run dev
 # or
+yarn install
 yarn dev
 ```
 
 Open [http://localhost:3000](http://localhost:3000) with your browser to see the result.
+d
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,31 @@
 import type { NextPage } from "next";
 
+import React, { Suspense } from "react";
+import { Canvas } from "@react-three/fiber";
+import { OrbitControls, Loader } from "@react-three/drei";
+import { Sphere } from "../components/Sphere";
+
 const Home: NextPage = () => {
   return (
-    <div className="w-full min-h-screen bg-blue-50 flex items-center justify-center">
-      <h1 className="font-bold text-4xl">Hello world!</h1>
+    <div className="h-screen cursor-pointer bg-gradient-to-tr from-blue-100 to-sky-300">
+      <Canvas
+        frameloop="demand"
+        dpr={[1, 2]}
+        camera={{ position: [0, 0, 5], fov: 45 }}
+      >
+        <spotLight
+          intensity={1}
+          angle={0.2}
+          penumbra={1}
+          position={[5, 15, 10]}
+        />
+        <ambientLight intensity={0.5} />
+        <Suspense fallback={null}>
+          <Sphere />
+        </Suspense>
+        <OrbitControls />
+      </Canvas>
+      <Loader />
     </div>
   );
 };
